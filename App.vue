2b8c06cf5f086f81f7f5fc89fbd3310ea5f3f7d7
hnxylc8818 <victory1993>
2019-02-24 18:00:15 +0800

    添加登录页面

--- App.vue
+++ App.vue
@@ -14,4 +14,19 @@
 
 <style>
 	/*每个页面公共css */
+	@import "common/base.scss";
+
+    .container {
+        height: 100%;
+        padding: px2rpx(18) px2rpx(18) px2rpx(35);
+    }
+    button::after{
+      border: none;
+    }
+    button:active{
+      opacity: 0.9;
+    }
+    .shadow{
+      box-shadow: 0 0 px2rpx(10) #E8E8E8;
+    }
 </style>
---
+++ pages/login/login.vue
@@ -0,0 +1,212 @@
+<template>
+	<view class="login">
+		<text class="logo">海马</text>
+		<text class="logo-desc">海外就医，马上到！</text>
+		<view class="form-view">
+			<form bindsubmit="formSubmit">
+				<view class="common phone">
+					<image src="../../static/img/icon_phone.png" />
+					<view class="line"></view>
+					<input name="phone" placeholder="手机号码" placeholder-style="color:#CECECE" type="number" confirm-type="next"
+					 maxlength="11" />
+				</view>
+				<view class="common code">
+					<image src="../../static/img/icon_lock.png" />
+					<view class="line"></view>
+					<input name="code" :value="codeValue" placeholder="验证码" placeholder-style="color:#CECECE" type="number"
+					 confirm-type="done" maxlength="6" />
+					<view class="line"></view>
+					<button id="code" :disabled="codeDisabled" :loading="codeLoading" form-type="submit" class="send-code">{{codeText}}</button>
+				</view>
+				<button id="login" :disabled="loginBtnLoading" :loading="loginBtnLoading" form-type="submit" class="login-btn">登录</button>
+			</form>
+		</view>
+		<view class="wechat-login" @click="bindWxLogin">
+			<image src="../../static/img/icon_wechat.png" />
+			<text>微信登录</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				loginBtnLoading: false,
+				currentTime: 60,
+				codeText: "发送验证码",
+				codeDisabled: false,
+				codeLoading: false,
+				interval: null,
+				codeValue: '',
+			};
+		},
+		methods: {
+			formSubmit(e) {
+				let targetID = e.detail.target.id;
+				let phoneNumber = e.detail.value.phone;
+				if (targetID == "code") {
+					// 获取验证码
+					if (!this.checkPhoneNumber(phoneNumber)) {
+						return;
+					}
+					// 调用发送验证码接口
+					this.sendSmsCode(phoneNumber)
+				} else {
+					// 登录
+					if (!this.checkPhoneNumber(phoneNumber)) {
+						return;
+					}
+					let smsCode = e.detail.value.code;
+					if (!smsCode) {
+						wx.showToast({
+							title: '请输入验证码',
+							icon: 'none'
+						});
+						return;
+					}
+					// 调用登录
+					this.loginBtnLoading = true;
+					let loginData = {
+						"telephone": phoneNumber,
+						"smscode": smsCode,
+					};
+					let t = this;
+					this.sendRequest(httpUrl.login, 'PUT', loginData, res => {
+						let accessToken = res.accessToken;
+						wx.setStorageSync('accessToken', accessToken);
+						this.loginBtnLoading = false;
+						if (this.interval) {
+							clearInterval(this.interval);
+						}
+						wx.showToast({
+							title: '登录成功'
+						});
+						let pages = getCurrentPages();
+						let prevPage = pages[pages.length - 2]; //上一个页面
+						if (prevPage) {
+							// console.log("prevPage",prevPage);
+							prevPage.onLoad(prevPage.options);
+						} else {
+							console.log("上一个页面实例为空,跳转到index");
+							t.$redirect("index");
+						}
+						this.$apply();
+						wx.navigateBack();
+					}, err => {
+						this.loginBtnLoading = false;
+						this.$apply();
+					}, false)
+				}
+			},
+			bindWxLogin() {
+				wx.showToast({
+					title: '即将上线，敬请期待',
+					icon: 'none'
+				});
+			}
+		},
+	}
+</script>
+
+<style lang="scss">
+	@import "../../common/base.scss";
+
+	.login {
+		display: flex;
+		flex-direction: column;
+
+		.logo {
+			margin-left: px2rpx(35);
+			margin-top: px2rpx(61);
+			color: #FE9F05;
+			font-size: px2rpx(43);
+			font-weight: 600;
+		}
+
+		.logo-desc {
+			margin-top: px2rpx(18);
+			margin-left: px2rpx(35);
+			color: #333333;
+			font-size: px2rpx(21);
+			font-weight: 100;
+		}
+
+		.form-view {
+			margin-top: px2rpx(52);
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+
+			.common {
+				width: px2rpx(335);
+				height: px2rpx(49);
+				border: 1px solid #A6A6A6;
+				border-radius: px2rpx(24);
+				display: flex;
+				align-items: center;
+
+				input {
+					margin-left: px2rpx(11);
+					font-size: px2rpx(14);
+					color: #333333;
+				}
+
+				image {
+					margin-left: px2rpx(19);
+					width: px2rpx(22);
+					height: px2rpx(22);
+				}
+
+				.line {
+					margin-left: px2rpx(9);
+					width: 1px;
+					height: px2rpx(15);
+					background-color: #A6A6A6;
+				}
+			}
+
+			.code {
+				margin-top: px2rpx(20);
+
+				.send-code {
+					background-color: transparent;
+					padding: 0;
+					color: #FE9F05;
+					font-size: px2rpx(14);
+					font-weight: 600;
+					line-height: px2rpx(18);
+				}
+			}
+
+			.login-btn {
+				margin-top: px2rpx(20);
+				width: px2rpx(335);
+				height: px2rpx(49);
+				line-height: px2rpx(49);
+				border-radius: px2rpx(24);
+				background-color: #FE9F05;
+				color: white;
+				font-size: px2rpx(18);
+			}
+		}
+
+		.wechat-login {
+			margin-top: px2rpx(49);
+			align-self: center;
+			display: flex;
+			align-items: center;
+
+			image {
+				width: px2rpx(25);
+				height: px2rpx(22);
+			}
+
+			text {
+				margin-left: px2rpx(7);
+				color: #333333;
+				font-size: px2rpx(18);
+			}
+		}
+	}
+</style>
